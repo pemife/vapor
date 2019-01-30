80fdc3a9a8b70a3b334dedd3bf86651129f00f33
Pepe Millan Fernandez <jose.millan@iesdonana.org>
2019-01-30 15:12:22 +0100

    vapor primer commit

--- db/vapor.sql
+++ db/vapor.sql
@@ -2,3 +2,19 @@
 -- Archivo de base de datos --
 ------------------------------
 
+CREATE TABLE usuarios
+(
+    id          BIGSERIAL     PRIMARY KEY
+  , nombre      VARCHAR(32)   NOT NULL UNIQUE
+  , password    VARCHAR(60)   NOT NULL UNIQUE
+);
+
+CREATE TABLE juegos
+(
+    id          BIGSERIAL     PRIMARY KEY
+  , titulo      VARCHAR(255)  NOT NULL UNIQUE
+  , descripcion TEXT
+  , precio      NUMERIC(5,2)  CONSTRAINT ck_juego_precio_positivo
+                              CHECK (coalesce(precio, 0) >= 0)
+  , 
+);
